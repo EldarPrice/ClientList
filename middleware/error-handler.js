d8a4a1db4512da2e1c594f961916f63471d3d949
Eldar Shiriyazdanov <119824346+EldarPrice@users.noreply.github.com>
2023-05-04 15:12:40 -0500

    fixed and added picture

--- middleware/error-handler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = async (err, req, res, next) => {
-  console.log(err)
-  return res.status(500).send({ msg: 'Something went wrong, please try again' })
-}
-
-module.exports = errorHandler
